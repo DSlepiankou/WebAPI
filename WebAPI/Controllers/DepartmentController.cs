5d987a24e539a3795fa63ea8e0e16700eec07a81
Slepiankou Dzmitry <Slepenkov95@mail.ru>
2020-12-09 15:43:56 +0300

    init rep
    init webAPI project

---
+++ WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult GetAllDepartment()
+        {
+
+        }
+    }
+}
